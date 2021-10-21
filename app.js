eea16e60e01339606ba2b58afcf78261c45941f4
monseigneur0 <monseigneur0@naver.com>
2021-10-21 16:50:17 +0900

    10.21 new

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const port = 8000;
 
 app.set( "view engine", "ejs");
 app.set( "views", __dirname + "/views" );
+//dirname 절대경로로 현재위치 지정 뒤에 폴더이름밑
+app.use( '/static', express.static( __dirname + '/statics' ));
 
-app.use( '/static', express.static( __dirname + 'public' ));
-
-app.get( '/', (req,res)   => {
+app.get( '/', ( req, res )   => {
     res.send( "안녕");
 });
 
@@ -19,19 +19,18 @@ app.listen( port, () => {
     console.log("8000!");
 } );
 
-app.get( '/test', (req,res)   => {
+app.get( '/test', ( req, res )   => {
     res.render( "test", { para1 : 1, para2 : '코딩온' } );
 });
-app.get( '/form', ( req, res) => {
-    console.log( req.query.id );
-    res.render('form');
-});
-app.post( '/form22', ( req, res) => {
-    console.log(req.body.name );
-    console.log( "post form 들어옴");
-})
 
-app.get( '/', (req,res)   => {
-    res.send( "안녕");
+app.get( '/form', ( req, res )   => {
+    console.log(req.query.id)
+    res.render( 'form' );
 });
+
+app.post( '/form22', ( req, res ) => {
+    console.log( "Post form 들어옴");
+    res.send( "hi");
+});
+
 
---
+++ mysql_test.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var mysql = require( 'mysql' );
+
+var conn = mysql.createConnection({
+	user: 'root',
+	password: 'rlaxkrgh1!',
+	database: 'ssac'
+});
+
+app.listen( 8000, function () {
+	console.log( conn );
+	console.log( conn.state );
+
+    console.log( "Listening on *:3000" );
+});
